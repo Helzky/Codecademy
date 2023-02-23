8707c50b1545d59fa0b9161771bf0e0474379196
Nick Tiwana <106453137+Helzky@users.noreply.github.com>
2023-02-23 02:03:42 -0500

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+void myFunction(){
+
+    std::cout << "There's so many things to do." << "\n";
+}
+
+int main() {
+    myFunction();
+    return 0;
+}
---
+++ multifileprograms.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+
+// Add declarations here:
+int tenth_power();
+double average();
+bool is_palindrome();
+
+double average(double num1, double num2) {
+  return (num1 + num2) / 2;
+}
+
+int tenth_power(int num) {
+  return pow(num, 10);
+}
+
+bool is_palindrome(std::string text) {
+  std::string reversed_text = "";
+  
+  for (int i = text.size() - 1; i >= 0; i--) {
+    reversed_text += text[i];
+  }
+  
+  if (reversed_text == text) {
+    return true;
+  }
+  
+  return false;
+}
+
+int main() {
+  
+  std::cout << is_palindrome("racecar") << "\n";
+  std::cout << tenth_power(3) << "\n";
+  std::cout << average(8.0, 19.0) << "\n";
+  
+}
